67041d42db7adfce737581cc43491cb6160e1214
Dylan Wooters <dylan@dylanwooters.com>
2015-05-20 21:40:38 -0400

    initial commit

---
+++ OHAPICSharp/OHDrives.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Net;
+using System.Configuration;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OHAPICSharp
+{
+    public class OHDrives
+    {
+        private string userID;
+        private string secretKey;
+
+        //Constructors
+        public OHDrives()
+        {
+            this.userID = ConfigurationManager.AppSettings["OHUserId"];
+            this.secretKey = ConfigurationManager.AppSettings["SecretKey"];
+        }
+
+        public OHDrives(string userID, string secretKey)
+        {
+            this.userID = userID;
+            this.secretKey = secretKey;
+        }
+
+        //Return a Drive 
+        public async Task<OHDrive> Get(string driveID)
+        {
+            string json = "";
+
+            using (HttpClient client = OHUtilities.CreateClient(userID, secretKey))
+            {
+                var response = await client.GetAsync("drives/515b36ec-a674-46a5-9778-00c2b80fee59/info/full");
+                json = await response.Content.ReadAsStringAsync();
+            }
+
+            //To do: add Json property attributes to match oh api response
+            return JsonConvert.DeserializeObject<OHDrive>(json);
+        }
+        
+    }
+}
---
+++ OHAPICSharp/OHUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+
+namespace OHAPICSharp
+{
+    public static class OHUtilities
+    {
+        public static HttpClient CreateClient(string userID, string secretKey)
+        {
+            HttpClient client = new HttpClient();
+
+            var auth = Convert.ToBase64String(Encoding.ASCII.GetBytes(userID + ":" + secretKey));
+            client.BaseAddress = new Uri(@"https://api-east1.openhosting.com/");
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", auth);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
