4bb4441123575fe1ff0effe5e7137bfe6fd0bcb3
Dylan Wooters <dylan@dylanwooters.com>
2015-10-12 00:29:08 -0400

    get and getall methods working for drive service. need to sort out size error in create method.

---
+++ OHAPICSharp/Entities/OHDrive.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OHAPICSharp
+{
+    public class OHDrive
+    {
+        [JsonProperty(PropertyName = "drive")]
+        public string DriveID { get; set; }
+        public string Name { get; set; }
+        public long Size { get; set; }
+        [JsonProperty(PropertyName = "claimed")]
+        public string Claim { get; set; }
+        public string Status { get; set; }
+        [JsonProperty(PropertyName = "encryption:cipher")]
+        public string Encryption { get; set; }
+        [JsonProperty(PropertyName = "user")]
+        public string UserID { get; set; }
+        [JsonProperty(PropertyName = "write:bytes")]
+        public long WriteBytes { get; set; }
+        [JsonProperty(PropertyName = "write:requests")]
+        public long WriteRequests { get; set; }
+        [JsonProperty(PropertyName = "read:bytes")]
+        public long ReadBytes { get; set; }
+        [JsonProperty(PropertyName = "read:requests")]
+        public long ReadRequests { get; set; }
+        //Leaving these out for now, since they are not returned in testing
+        //public List<string> Readers { get; set; }
+        //public List<string> Tags { get; set; }
+        //public List<string> Avoids { get; set; }
+    }
+}
---
+++ OHAPICSharp/Services/OHDriveService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Net;
+using System.Web;
+using System.Configuration;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OHAPICSharp
+{
+    public class OHDriveService
+    {
+        private string userID;
+        private string secretKey;
+        public const string urlBase = "drives/";
+
+        //Constructors
+        public OHDriveService()
+        {
+            this.userID = ConfigurationManager.AppSettings["OHUserId"];
+            this.secretKey = ConfigurationManager.AppSettings["OHSecretKey"];
+        }
+
+        public OHDriveService(string userID, string secretKey)
+        {
+            this.userID = userID;
+            this.secretKey = secretKey;
+        }
+
+        //Return a List of Drives 
+        public async Task<List<OHDrive>> GetAll()
+        {
+            string json = "";
+            List<OHDrive> drives = new List<OHDrive>();
+
+            using (HttpClient client = OHUtilities.CreateClient(userID, secretKey))
+            {
+                var url = urlBase + "list";
+                var response = await client.GetAsync(url);
+                json = await response.Content.ReadAsStringAsync();
+            }
+
+            dynamic driveList = JsonConvert.DeserializeObject(json);
+            foreach (var drive in driveList)
+            {
+                string driveId = drive.uuid;
+                drives.Add(Get(driveId).Result);
+            }
+
+            return drives;
+        }
+
+        //Return a Drive 
+        public async Task<OHDrive> Get(string driveID)
+        {
+            string json = "";
+
+            using (HttpClient client = OHUtilities.CreateClient(userID, secretKey))
+            {
+                var url = string.Format("{0}{1}/info/full", urlBase, driveID);
+                var response = await client.GetAsync(url);
+                json = await response.Content.ReadAsStringAsync();
+            }
+
+            return JsonConvert.DeserializeObject<OHDrive>(json);
+        }
+
+        //Create a Drive 
+        public async Task<OHDrive> Create(OHDrive drive)
+        {
+            string json = "";
+            var values = new Dictionary<string, string>();
+
+            //name and size are required
+            if (string.IsNullOrEmpty(drive.Name) || drive.Size == 0)
+                throw new Exception("Name and Size are required for drive creation");
+
+            values["name"] = drive.Name;
+            values["size"] = drive.Size.ToString();
+            if (drive.UserID != null)
+                values["user"] = drive.UserID;
+            if (drive.Encryption != null)
+                values["encryption:cipher"] = drive.Encryption;
+            
+            var content = new FormUrlEncodedContent(values);
+
+            using (HttpClient client = OHUtilities.CreateClient(userID, secretKey))
+            {
+                var url = urlBase + "create";
+                var response = await client.PostAsync(url, content);
+                json = await response.Content.ReadAsStringAsync();
+            }
+
+            return JsonConvert.DeserializeObject<OHDrive>(json);
+        }
+    }
+}
